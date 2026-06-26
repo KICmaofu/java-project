91b33f4506abcff7dd33aa9a51b386101c91f807
T1.winds <153503409+KICmaofu@users.noreply.github.com>
2026-06-26 21:41:58 +0800

    1

---
+++ srping-ai/src/main/java/com/example/srpingai/SrpingAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.srpingai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingAiApplication.class, args);
+    }
+
+}
---
+++ srping-ai/src/test/java/com/example/srpingai/SrpingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srpingai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
