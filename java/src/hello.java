3142af2615b88826f03f6931c093319c4c339bc9
T1.winds <153503409+KICmaofu@users.noreply.github.com>
2026-07-01 18:03:25 +0800

    1

--- java/src/hello.java
+++
@@ -1,8 +0,0 @@
-public class hello {
-    public static void main(String[] args) {
-    System.out.println(String.format("Hello world!"));
-    for (int i = 0; i < 10; i++) {
-        System.out.println("This is iteration number: " + i);
-    }
-}
-}
