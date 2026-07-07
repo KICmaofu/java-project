8ebddbacc5e58594a365bf2c992405b3fbcd6f66
KICmaofu <153503409+KICmaofu@users.noreply.github.com>
2026-07-07 23:50:10 +0800

    1

--- java/src/Scanner.java
+++
@@ -1,9 +0,0 @@
-public class Scanner {
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("请输入一个整数：");
-        int number = scanner.nextInt();
-        System.out.println("您输入的整数是：" + number);
-        scanner.close();
-    }
-}
